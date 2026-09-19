6aff0382a2ea1ff8b9f6ce353334dc929ed918cc
IPEACE04 <peerapat.cd@gmail.com>
2026-09-19 20:22:33 +0700

    feat(master-data): add product reference APIs

---
+++ lib/units-of-measure/types.ts
@@ -0,0 +1,2 @@
+export type UnitOfMeasureDto={id:string;organizationId:string;code:string;name:string;decimalScale:number;isActive:boolean;version:number;createdAt:string;updatedAt:string};
+export type UnitOfMeasureInput={code:string;name:string;decimalScale:number;isActive:boolean};export type UnitOfMeasureUpdateInput=Partial<UnitOfMeasureInput>&{version:number};
